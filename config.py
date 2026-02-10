185ad7e90a6392a889878fc8ed3ea7f92bb7228f
Abraham Aisosa <112825477+aboyz1@users.noreply.github.com>
2026-02-10 02:16:58 +0100

    Update GCP storage bucket environment variable
    Remove default value for GCP storage bucket.

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Settings:
     gcp_project_id = os.getenv("GCP_PROJECT_ID")
     gcp_region = os.getenv("GCP_REGION")
     gar_repository_name = os.getenv("GAR_REPOSITORY_NAME")
-    gcp_storage_bucket = os.getenv("GCP_STORAGE_BUCKET", "perd-fd33f.firebasestorage.app")
+    gcp_storage_bucket = os.getenv("GCP_STORAGE_BUCKET")
 
 settings = Settings()
